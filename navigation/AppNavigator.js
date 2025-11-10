e23d0a57d285d8468a711f321a4f4f44c11b6deb
raunakfr <cabc4619@Gmail.com>
2025-11-10 22:43:33 +0530

    Base App

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Dashboard from '../screens/Dashboard';
+import DailyInput from '../screens/DailyInput';
+import Prediction from '../screens/Prediction';
+import Weekly from '../screens/Weekly';
+import Login from '../screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Dashboard" component={Dashboard}/>
+        <Stack.Screen name="DailyInput" component={DailyInput}/>
+        <Stack.Screen name="Prediction" component={Prediction}/>
+        <Stack.Screen name="Weekly" component={Weekly}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
